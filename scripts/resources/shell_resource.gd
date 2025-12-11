ebbc87b13e473cc8bb5c624c773d22fe25463584
BryceHanke <Crazy707707@gmail.com>
2025-12-10 18:45:01 -0800

    yes

--- scripts/resources/shell_resource.gd
+++ scripts/resources/shell_resource.gd
@@ -2,7 +2,7 @@ extends Resource
 
 class_name ShellResource
 
-@export var mesh: ArrayMesh
+@export var mesh: SphereMesh
 @export var collider: Shape3D
 @export var friction: float = 0.5
 @export var bounce: float = 0.5
